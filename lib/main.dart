942eb4168548b10d6730a2cf82376d910c7a9d1b
Raj-Dave-1 <d21it170@charusat.edu.in>
2022-03-22 07:12:13 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,47 @@
+// 🚩 Dada Ki Jay Ho 🚩
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              color: Colors.red,
+              width: 100,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.amber,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+            ),
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
